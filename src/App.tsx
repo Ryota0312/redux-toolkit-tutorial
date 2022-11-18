554f6ca5212ac382aafc03e8a5ecbbd2ae0b7594
Ryota Nishi <ryota-nishi@cybozu.co.jp>
2022-11-18 10:34:08 +0900

    Quick start example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styled from "styled-components";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "./app/store";
+import {decrement, increment, incrementByAmount} from "./features/counter/counterSlice";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inputNum, setInputNum] = useState(0)
+
+    const count = useSelector((state: RootState) => state.counter.value)
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+            <Title>
+                count: {count}
+            </Title>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <input type={'text'} value={inputNum} onChange={(e) => setInputNum(Number(e.target.value))} />
+            <button onClick={() => dispatch(incrementByAmount(inputNum))}>Increment By Amount</button>
+        </div>
+    );
 }
 
+const Title = styled.div`
+  color: red;
+`
 export default App;
